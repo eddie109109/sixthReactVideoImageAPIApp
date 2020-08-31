e3a1875e2e0f4b6ab22a408d2fbb671e5fb30abe
jundic <jundi_chen@sfu.ca>
2020-08-30 18:06:22 -0700

    Updates

---
+++ precache-manifest.393e531eb3688dabb0b0af3e4a89eda1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3d2c538317949a24bcda48f21bcebb",
+    "url": "/sixthReactVideoImageAPIApp/index.html"
+  },
+  {
+    "revision": "a3e9f8f5b1a7593e0fcd",
+    "url": "/sixthReactVideoImageAPIApp/static/js/2.c3ae9fb3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/sixthReactVideoImageAPIApp/static/js/2.c3ae9fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2613462e15ea5369c56d",
+    "url": "/sixthReactVideoImageAPIApp/static/js/main.ee73851d.chunk.js"
+  },
+  {
+    "revision": "c5cd024dd5bccfa76ec7",
+    "url": "/sixthReactVideoImageAPIApp/static/js/runtime-main.267431ad.js"
+  }
+]);
